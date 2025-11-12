448f03be58e71606e7e95dfc5fe53ce703d2f9f3
code-with-wandera <collinswandera560@gmail.com>
2025-11-12 22:08:30 +0300

    Updated Login page

--- Client/login/src/Login.jsx
+++ Client/login/src/Login.jsx
@@ -4,7 +4,51 @@ import React from 'react'
 const Login=() =>{
   return (
     <>
-     
+      <div className='d-flex justify-content-center align-items-center bg-secondary vh-100'>
+      <div className='bg-white p-4 rounded w-25 shadow'>
+        <h2 className='mb-4 text-center'>Sign-In</h2>
+
+        <form>
+          <div className='mb-3'>
+            <label htmlFor='email' className='form-label'>
+              <strong>Email</strong>
+            </label>
+            <input
+              type='email'
+              id='email'
+              placeholder='Enter Email'
+              name='email'
+              className='form-control rounded-0'
+            />
+          </div>
+
+          <div className='mb-3'>
+            <label htmlFor='password' className='form-label'>
+              <strong>Password</strong>
+            </label>
+            <input
+              type='password'
+              id='password'
+              placeholder='Enter Password'
+              name='password'
+              className='form-control rounded-0'
+            />
+          </div>
+
+          <button type='submit' className='btn btn-success w-100 rounded-0'>
+            Sign Up
+          </button>
+
+          <p className='mt-3 text-center'>You do not have an account and you agree to our terms and condition? Create an account here</p>
+          <button
+            type='button'
+            className='btn btn-light border w-100 rounded-0'
+          >
+            Create Account
+          </button>
+        </form>
+      </div>
+    </div>
     </>
   )
 }
