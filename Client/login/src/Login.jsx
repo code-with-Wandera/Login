b2f14ad41c48aa2208e1c7befbf2fcda953afe03
code-with-wandera <collinswandera560@gmail.com>
2025-11-23 20:42:11 +0300

    initial commit

--- Client/login/src/Login.jsx
+++ Client/login/src/Login.jsx
@@ -1,56 +1,39 @@
-
 import React from 'react'
 import { Link } from 'react-router-dom'
-const Login=() =>{
+
+const Login = () => {
   return (
     <>
       <div className='d-flex justify-content-center align-items-center bg-secondary vh-100'>
-      <div className='bg-white p-4 rounded w-25 shadow'>
-        <h2 className='mb-4 text-center'>Sign-In</h2>
+        <div className='bg-white p-4 rounded w-25 shadow'>
+          <h2 className='mb-4 text-center'>Sign-In</h2>
 
-        <form>
-          <div className='mb-3'>
-            <label htmlFor='email' className='form-label'>
-              <strong>Email</strong>
-            </label>
-            <input
-              type='email'
-              id='email'
-              placeholder='Enter Email'
-              name='email'
-              className='form-control rounded-0'
-            />
-          </div>
+          <form>
+            <div className='mb-3'>
+              <label htmlFor='email' className='form-label'><strong>Email</strong></label>
+              <input type='email' id='email' placeholder='Enter Email' name='email' className='form-control rounded-0' />
+            </div>
 
-          <div className='mb-3'>
-            <label htmlFor='password' className='form-label'>
-              <strong>Password</strong>
-            </label>
-            <input
-              type='password'
-              id='password'
-              placeholder='Enter Password'
-              name='password'
-              className='form-control rounded-0'
-            />
-          </div>
+            <div className='mb-3'>
+              <label htmlFor='password' className='form-label'><strong>Password</strong></label>
+              <input type='password' id='password' placeholder='Enter Password' name='password' className='form-control rounded-0' />
+            </div>
 
-          <button type='submit' className='btn btn-success w-100 rounded-0'>
-            Sign Up
-          </button>
+            <button type='submit' className='btn btn-success w-100 rounded-0'>
+              Login
+            </button>
 
-          <p className='mt-3 text-center'>You do not have an account and you agree to our terms and condition? Create an account here</p>
-          <Link to='/register'
-            type='button'
-            className='btn btn-light border w-100 rounded-0'
-          >
-            Create Account
-          </Link>
-        </form>
+            <p className='mt-3 text-center'>You do not have an account?</p>
+
+            <Link to='/register' className='btn btn-light border w-100 rounded-0'>
+              Create Account
+            </Link>
+          </form>
+
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- Client/login/src/Register.jsx
+++ Client/login/src/Register.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import axios from axios
+import axios from "axios";
+
 const Register = () => {
 
-  const [values, setValues]= useState({
-    name:"",
-    email:"",
-    password:""
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
   });
 
-  const handleSubmit =(event)=>{
-    event.preventDegault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
     axios.post('http://localhost:8081/register', values)
-    .then(res =>console.log(res))
-    .then(err =>console.log(err));
-  }
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className='d-flex justify-content-center align-items-center bg-secondary vh-100'>
       <div className='bg-white p-4 rounded w-25 shadow'>
@@ -23,43 +25,37 @@ const Register = () => {
 
         <form onSubmit={handleSubmit}>
           <div className='mb-3'>
-            <label htmlFor='name' className='form-label'>
-              <strong>Name</strong>
-            </label>
+            <label htmlFor='name' className='form-label'><strong>Name</strong></label>
             <input
               type='text'
               id='name'
               placeholder='Enter Name'
               name='name'
-              onChange={e => setValues({...values, name: e.target.value})}
+              onChange={e => setValues({ ...values, name: e.target.value })}
               className='form-control rounded-0'
             />
           </div>
 
           <div className='mb-3'>
-            <label htmlFor='email' className='form-label'>
-              <strong>Email</strong>
-            </label>
+            <label htmlFor='email' className='form-label'><strong>Email</strong></label>
             <input
               type='email'
               id='email'
               placeholder='Enter Email'
               name='email'
-              onChange={e => setValues({...values, email: e.target.value})}
+              onChange={e => setValues({ ...values, email: e.target.value })}
               className='form-control rounded-0'
             />
           </div>
 
           <div className='mb-3'>
-            <label htmlFor='password' className='form-label'>
-              <strong>Password</strong>
-            </label>
+            <label htmlFor='password' className='form-label'><strong>Password</strong></label>
             <input
               type='password'
               id='password'
               placeholder='Enter Password'
               name='password'
-              onChange={e => setValues({...values, password: e.target.value})}
+              onChange={e => setValues({ ...values, password: e.target.value })}
               className='form-control rounded-0'
             />
           </div>
@@ -69,10 +65,7 @@ const Register = () => {
           </button>
 
           <p className='mt-3 text-center'>Already have an account?</p>
-          <Link to='/login'
-            type='button'
-            className='btn btn-light border w-100 rounded-0'
-          >
+          <Link to='/login' className='btn btn-light border w-100 rounded-0'>
             Login Here
           </Link>
         </form>
